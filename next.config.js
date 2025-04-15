a9705474ab6696ac266d697414db5d6dddbae479
cloudtech63 <waiming1210@gmail.com>
2025-04-15 16:22:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    // 添加路径别名
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components')
+    };
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
